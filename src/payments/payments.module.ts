034698007b5990d360b6091d4506cbee7e883968
josephmarienguessansigmund-cpu <josephmarie.nguessan.sigmund@gmail.com>
2026-03-28 11:48:35 +0000

    Update payments.module.ts

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,1 +1,8 @@
+import { Module } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
 
+@Module({
+  providers: [PaymentsService],
+  exports: [PaymentsService], // Cela permet aux autres modules d'utiliser le paiement
+})
+export class PaymentsModule {}
